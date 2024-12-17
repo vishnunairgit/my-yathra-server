ef273a10fdb8c6babcfe9e54da8c8d6acc2f1e4b
vishnu nair <infovishnunair@gmail.com>
2024-12-17 21:17:52 +0530

    w

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,31 +1,4 @@
 
-// const multer = require('multer');
-
-// const fileStorage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//         callback(null, 'public/UserFiles');
-//     },
-//     filename: (req, file, callback) => {
-//         callback(null, Date.now() + "-" + file.originalname);
-//     }
-// });
-
-// const fileFilter = (req, file, callback) => {
-//     const allowedTypes = ['image/jpeg', 'image/png',];
-//     if (!allowedTypes.includes(file.mimetype)) {
-//         return callback(new Error('Invalid file type'), false);
-//     }
-//     callback(null, true);
-// };
-
-// const upload = multer({ storage: fileStorage, fileFilter  }).fields([
-//     { name: 'TripFile', maxCount: 1 },
-//     { name: 'logoFile', maxCount: 1 },
-//     { name: 'imageFile', maxCount: 1 },
-// ]);
-
-// module.exports = upload;
-
 
 const fs = require('fs');
 const multer = require('multer');
