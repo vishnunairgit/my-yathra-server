a8073b9eb2fabb20b72a2ec469aafc357c9d03a6
vishnu nair <infovishnunair@gmail.com>
2025-01-01 19:08:32 +0530

    db

--- config/Db.js
+++ config/Db.js
@@ -1,13 +1,14 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        const dbUrl = process.env.MONGODB_URL;
+        // const dbUrl = process.env.MONGODB_URL;
+        const dbUrl = process.env.MONGODB_URL || 'mongodb+srv://infovishnuac:c6OnStRwRE6FeT1W@myyathra.dshod.mongodb.net/?retryWrites=true&w=majority&appName=myyathra';
         if (!dbUrl) {
             throw new Error('MONGODB_URL is not defined in the environment variables.');
         }
-        
+
         const connection = await mongoose.connect(dbUrl);
         console.log('my yathra MongoDB connected');
     } catch (error) {
@@ -16,41 +17,3 @@ const connectToDatabase = async () => {
 }
 
 module.exports = connectToDatabase;
-
-
-
-
-
-
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-
-// // Function to connect to MongoDB
-// const connectToDatabase = async () => {
-//     try {
-//         // Get the MongoDB URI from environment variables
-//         const dbUrl = process.env.MONGODB_URL;
-
-//         // Connect to MongoDB with necessary options
-//         await mongoose.connect(dbUrl, {
-//             useNewUrlParser: true,     // Ensures the new URL parser is used
-//             useUnifiedTopology: true, // Ensures the new server discovery and monitoring engine is used
-//         });
-
-//         console.log('MongoDB connected successfully');
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error.message);
-//         process.exit(1); // Exit process with failure if the connection fails
-//     }
-// };
-
-// module.exports = connectToDatabase;
-
-
-
-
-
-
-
-
-
