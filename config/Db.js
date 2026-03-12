0fe264f37ea6bffb591a69ba97b579718ab4cc24
vishnu nair <infovishnunair@gmail.com>
2026-03-12 16:37:11 +0530

    mongo db chnages

--- config/Db.js
+++ config/Db.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        // const dbUrl = process.env.MONGODB_URL;
-        const dbUrl = process.env.MONGODB_URL || 'mongodb+srv://infovishnuac:c6OnStRwRE6FeT1W@myyathra.dshod.mongodb.net/?retryWrites=true&w=majority&appName=myyathra';
+    
+        const dbUrl = process.env.MONGODB_URL || 'mongodb+srv://infovishnuac:7hhfpBDzciYFn938@myyathra.dshod.mongodb.net/?appName=myyathra';
+        // const dbUrl = process.env.MONGODB_URL || 'mongodb+srv://infovishnuac:c6OnStRwRE6FeT1W@myyathra.dshod.mongodb.net/?retryWrites=true&w=majority&appName=myyathra';
         if (!dbUrl) {
             throw new Error('MONGODB_URL is not defined in the environment variables.');
         }
